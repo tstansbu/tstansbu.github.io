b1f9f9c27549e9662690f1170a15fd948e5fe149
troy <tstansbu@umich.edu>
2019-11-13 16:51:16 -0500

    Update script.js

--- portfolio/javascript/script.js
+++ portfolio/javascript/script.js
@@ -10,3 +10,10 @@ function minfun(x) {
         x.value = "true";
     }
 }
+
+browser.runtime.onMessage.addListener(message => {
+  console.log("background: onMessage", message);
+
+  // Add this line:
+  return Promise.resolve("Dummy response to keep the console quiet");
+});
